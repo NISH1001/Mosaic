5c1cd6463ff4a1204eb7e47053c9c918e43c8791
NISH1001 <nish_play1001@yahoo.com>
2015-01-09 14:50:35 +0545

    comments added for Mat class

--- include/Mat.h
+++ include/Mat.h
@@ -1,13 +1,26 @@
 #pragma once
 
 #include <Matrix.h>
+#include <Point2D.h>
+
+/*
+	Uses Matrix class
+*/
 
 class Mat
 {
 public:
+	// return 3x3 identity
 	static Matrix Mat3(void);
-	static Matrix Mat4(void);
+
+	// return 4x4 identity matrix
+	static Matrix Mat4(void);	
+
+	//return 3x1 homogeneous matrix for given 2D point
 	static Matrix Vec2(double x, double y);
+	static Matrix Vec2(const Point2D & p);
+
+	//return 4x1 homogeneous matrix for given 3D point
 	static Matrix Vec3(double x, double y,double z);
 private:
 
--- src/Mat.cpp
+++ src/Mat.cpp
@@ -28,6 +28,15 @@ Matrix Mat::Vec2(double x, double y)
 	return mat31;
 }
 
+Matrix Mat::Vec2(const Point2D & p)
+{
+	Matrix mat31(3,1);
+	mat31[0][0] = p.x;
+	mat31[1][0] = p.y;
+	mat31[2][0] = 1;
+	return mat31;
+}
+
 Matrix Mat::Vec3(double x, double y,double z)
 {
 	Matrix mat41(4,1);
