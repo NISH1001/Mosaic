3519c58c18302179adf9c37e48895afa720e8bd2
NISH1001 <nish_play1001@yahoo.com>
2015-01-09 16:22:50 +0545

    Mat class : oveloaded functions

--- include/Mat.h
+++ include/Mat.h
@@ -13,15 +13,26 @@ public:
 	// return 3x3 identity
 	static Matrix Mat3(void);
 
-	// return 4x4 identity matrix
-	static Matrix Mat4(void);	
+	//return 3x3 matrix with all elements
+	static Matrix Mat3(double x);
 
-	//return 3x1 homogeneous matrix for given 2D point
+	// return 4x4 identity matrix
+	static Matrix Mat4(void);
+	//return 4x4 matrix with all elements
+	static Matrix Mat4(double x);	
+
+	//return 2x1 matrix for given 2D point
 	static Matrix Vec2(double x, double y);
 	static Matrix Vec2(const Point2D & p);
 
-	//return 4x1 homogeneous matrix for given 3D point
+	//return 3x1 matrix for given 3D point
 	static Matrix Vec3(double x, double y,double z);
+	static Matrix Vec3(const Matrix & vec2, double z);
+
+	//return 4x1 matrix
+	static Matrix Vec4(double x, double y, double z, double w);
+	static Matrix Vec4(const Matrix & vec3, double w);
+
 private:
 
 };
