763ca7756ce3b51afb8e95b3182b7207488a9777
NISH1001 <nish_play1001@yahoo.com>
2015-01-21 09:20:19 +0545

    Vertex and index buffer implemented

--- src/rendertest.cpp
+++ src/rendertest.cpp
@@ -15,23 +15,44 @@ void FragmentShader(Point2D& p)
 	renderer.SetPixel(p.x, p.y, ColorRGBA(att[0],att[1],att[2],0));
 }
 
+Point2D vertices [] = { {Point2D(100,200)},
+						{Point2D(0,200)},
+						{Point2D(50,20)},
+						{Point2D(400,0)},
+						{Point2D(600,100)},
+						{Point2D(200,200)},
+					};
+int numvertices = sizeof(vertices)/sizeof(Point2D);
+
+int indices[] = {
+				0,1,2,
+				3,4,5,
+				5,3,1
+			};
+int numindices = sizeof(indices)/sizeof(int);
 
 void Render()
 {
-	Point2D p1(100,200);
-	Point2D p2(0, 200);
-	Point2D p3(50, 20);
 	Vec3 v1(255,0,0);
 	Vec3 v2(111,255,0);
 	Vec3 v3(0,200,255);
-	p1.attributes[0] = v1;
-	p2.attributes[0] = v2;
-	p3.attributes[0] = v3;
+	
 	Rasterizer rast;
 	float *t;
 	int w = 800;
 	int h = 600;
-	rast.DrawTriangle(p1,p2,p3,w,h, &FragmentShader, t);// test only
+
+	for(int i=0; i<numindices; i+=3)
+	{
+		Point2D p1 = vertices[indices[i]];
+		Point2D p2 = vertices[indices[i+1]];
+		Point2D p3 = vertices[indices[i+2]];
+		p1.attributes[0] = v1;
+		p2.attributes[0] = v2;
+		p3.attributes[0] = v3;
+		rast.DrawTriangle(p1, p2, p3,
+						w, h , &FragmentShader, t);
+	}
 
 }
 
